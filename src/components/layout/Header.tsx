a341dd402a62a0b970120f5bd07245582629af13
Sunnyg0211 <144601141+Sunnyg0211@users.noreply.github.com>
2026-01-17 00:23:17 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -27,7 +27,7 @@ export function Header() {
             <img
               src="/full_logo.png"
               alt="ZappTek Logo"
-              className="h-30 md:h-30 w-auto"
+              className="h-25 md:h-25 w-auto"
             />
           </Link>
 
