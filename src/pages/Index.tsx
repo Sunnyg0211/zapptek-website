e051a03b84d793bcb0708ad9379075daf865cc93
Sunnyg0211 <144601141+Sunnyg0211@users.noreply.github.com>
2026-01-19 07:11:24 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import { FeaturedProductsSection } from "@/components/home/FeaturedProductsSecti
 import { ServicesSection } from "@/components/home/ServicesSection";
 import { AMCSection } from "@/components/home/AMCSection";
 import { TestimonialsSection } from "@/components/home/TestimonialsSection";
-import { EnhancedTrustSection } from "@/components/home/CTASection";
+import { CTASection } from "@/components/home/CTASection";
 
 const Index = () => {
   return (
@@ -17,8 +17,9 @@ const Index = () => {
 
       <AMCSection />
 
-      <CTASection />
+      <TestimonialsSection />
 
+      {/* Final Call To Action Section */}
       <CTASection />
     </>
   );
